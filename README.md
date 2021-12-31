310810f09057b717a09140e44302c61b5e81d31d
zxc2007 <33582901+zxc2007@users.noreply.github.com>
2021-12-31 21:38:17 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# zxc2007.github.io
+# AccountChecker WebBased Api
+An Account Checker Website Source Code 
+
+# DISCLAIMER 
+This Tool is only for educational purposes 
+You'll be responsible yourself for whatever you do!!
+
+# Installation : 
+Method - 01:
+```
+Download The Source Code.
+Download xampp 
+Run The Script
+```
+Method - 02:
+```
+Download The Source Code.
+Get Any Website with Hosting
+Upload The Script and Unzip It.
+Then Use it From Your Website
+```
+
+# Feature
+```
+Express Vpn Code Checker [Requires Proxy]
+*When Some One Tries To Use Right Click.*
+*OR F12 Keys It will Not Allows Them To Open Inspect Option*
+```
+
+# How To Add Proxy?
+```
+while Using This Web Checker It Requires Proxy.
+Currently It works Based On Webshare.io Proxy.
+if You Want To Change or Load Fresh Proxies
+open ajax/express.php in Any Code Editor
+Change Proxy Credentials In Line 16.
+Simple If You Have Questions Ping me On Telegram.
+```
+
+# Join Us On Telegram
+*Click <a href="https://t.me/xBlacKxCoder">here</a> To Join My Telegram Channel .. <3*
+
+<h2>Screenshot</h2>
+<img src="https://i.imgur.com/vH7QfmQ.jpg" style="max-width:100%;">
+
+📧 Contact :
+------
+You Want Ask About All My Tools Or Buy Tools Private Text Me On : 
+```
+[+] Telegram : @xBlackx_Coder
+```
+
+<br>©2021 xBlackxCoder 
---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+/*
+       __________.__                 __            
+___  __\______   \  | _____    ____ |  | _____  ___
+\  \/  /|    |  _/  | \__  \ _/ ___\|  |/ /\  \/  /
+ >    < |    |   \  |__/ __ \\  \___|    <  >    < 
+/__/\_ \|______  /____(____  /\___  >__|_ \/__/\_ \
+      \/       \/          \/     \/     \/      \/
+        By xBlackx | @xBlackx_Coder
+*/
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Express VPN Code Checker</title>
+    <meta name="keywords" content="Express VPN Code Checker 2021 xBlackxCoder Code Checker Express VPN 2021 Working Express VPN Code">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-extended.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/colors.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/semi-dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/horizontal-menu.css">
+</head>
+<body class="horizontal-layout dark-layout 2-columns navbar-floating footer-static pace-done menu-expanded horizontal-menu">
+<!-- Atom Spinner !-->
+<div id="loader-wrapper" class="loader-wrapper">
+  <div class="atom-spinner">
+    <div class="spinner-inner">
+      <div class="spinner-line"></div>
+        <div class="spinner-line"></div>
+        <div class="spinner-line"></div>
+        <div class="spinner-circle">&#9679;</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Loading !-->
+<script type="text/javascript">
+setTimeout(function(){document.getElementById("loader-wrapper").style.display="none";}, 4000); 
+</script>
+<!-- Checker Main !-->
+<div class="card">
+    <div class="card-body"></div>
+    <h4>[Express Vpn Code Checker By @xBlackx_Coder]</h4>
+    <center>
+        <form method="post" action="">
+            <textarea class="form-control" id="listxB" name="Accounts" rows="5" placeholder="Your Codes Here."></textarea><br>
+            <div class="widget-body">
+                <button type="button" id="xB_Valids" class="btn btn-success">Valids [<span id="Valid_Count">0</span>]</button>
+                <button class="btn btn-info copy" onclick="selectElementContents(document.getElementById('Valids_Rz'));">Copy Valids</button>
+                <p id="Valids_Rz"></p>
+                <button type="button" id="xB_Invalids" class="btn btn-warning">Invalids [<span id="Invalids_Count">0</span>]</button>
+                <button class="btn btn-info copy" onclick="selectElementContents(document.getElementById('invalids'));">Copy Invalids</button>
+                <p id="invalids"></p>
+            </div>
+    <small>
+    Status: <span class="bagde badge-pill badge-primary" id="demo">Waiting...</span> - 
+    Live: <span class="badge badge-success" id="xB_Live">0</span>
+    Dead: <span class="badge badge-danger" id="xB_Deads">0</span>
+    Checked: <span class="badge badge-info" id="Checked">0</span>
+    Total: <span class="badge badge-warning" id="Total_Checked">0</span>
+    Time: <span class="badge badge-primary" id="xBTime">xBlackxCoder</span>
+    </small><br><br>    
+    <button type="button" id="startChk" onclick="start()" class="btn btn-info mr-sm-1 mb-1 mb-sm-0 waves-effect waves-light" type="submit" name="submit" value="start">Start</button>
+    <button type="button" id="stopChk" onclick="stop()" class="btn btn-danger mr-sm-1 mb-1 mb-sm-0 waves-effect waves-light" type="submit" name="submit" value="Stop">Stop</button>
+        </form><br>
+    <b>Join <a href="https://telegram.dog/xBlackxCoder"> Telegram Channel</a> For Latest Update 🔥🔥 !!</b>
+    <h2>Disclaimer</h2>
+<p>DISCLAIMER: This Source Code is only for educational purposes 
+You'll be responsible yourself for whatever you do!!.</p>    
+<p>Created By <a href="https://t.me/xBlackx_Coder">xBlackx</a> </p>
+
+<font color="red">  >> Powered By xBlackx <<  </font><br>
+<font color="red">  >> Thanks To Anonymous Hackez <<  </font><br>
+    </center>
+    <div class="footer">
+        Copyright &copy; 2021 <a href='https://xBlackxCoder.codes/'>xBlackxCoder</a>
+    </div>
+</div>
+<!-- Import The Scripts !-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="assets/jquery.js"></script>
+<script type="text/javascript"> function selectElementContents(e){var t,n,o=document.body;if(document.createRange&&window.getSelection){t=document.createRange(),(n=window.getSelection()).removeAllRanges();try{t.selectNodeContents(e),n.addRange(t)}catch(o){t.selectNode(e),n.addRange(t)}}else o.createTextRange&&((t=o.createTextRange()).moveToElementText(e),t.select());document.execCommand("Copy")}</script>
+<!-- End of The Scripts !-->
+</body>
+</html>
